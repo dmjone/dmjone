25de9054796bea35a724d7f485b388ec3ed4861e
Divya Mohan <divyamohan1993@users.noreply.github.com>
2024-02-18 13:36:26 +0530

    Update links and descriptions in var.js

--- edu/su/course/csu1162/var.js
+++ edu/su/course/csu1162/var.js
@@ -9,10 +9,13 @@ function get_menu_list(datetogen) {
                 codetype: "CSU1162 | Python Programming"
             },
             {
-            link: "lab/",
-            title: "Practical Python Programming Lab: Sharpen Your Coding Skills",
-            desc: "Elevate your programming expertise in the Python Lab with code CSU1162P. This hands-on lab course is designed to immerse you in the practical aspects of Python programming. From basic syntax to complex problem-solving, you'll learn to write efficient and effective Python code. Click to dive into interactive sessions that will bolster your coding proficiency, enhance your algorithm design capabilities, and prepare you for real-world programming challenges.",
-            codetype: "CSU1162P | Lab"
+                link: "https://colab.research.google.com/drive/1goYusw-6vB2d2qZtxo4V1kt4mmLBrN11?usp=sharing",
+                title: `Practical Python Programming Lab: Sharpen Your Coding Skills
+<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: inline; width: 2rem; height: 2rem;" width="30"  height="30" >
+    <g><path d="M4.54,9.46,2.19,7.1a6.93,6.93,0,0,0,0,9.79l2.36-2.36A3.59,3.59,0,0,1,4.54,9.46Z" style="" fill="#E8710A"></path><path d="M2.19,7.1,4.54,9.46a3.59,3.59,0,0,1,5.08,0l1.71-2.93h0l-.1-.08h0A6.93,6.93,0,0,0,2.19,7.1Z" style="" fill="#F9AB00"></path><path d="M11.34,17.46h0L9.62,14.54a3.59,3.59,0,0,1-5.08,0L2.19,16.9a6.93,6.93,0,0,0,9,.65l.11-.09" style="" fill="#F9AB00"></path><path d="M12,7.1a6.93,6.93,0,0,0,0,9.79l2.36-2.36a3.59,3.59,0,1,1,5.08-5.08L21.81,7.1A6.93,6.93,0,0,0,12,7.1Z" style="" fill="#F9AB00"></path><path d="M21.81,7.1,19.46,9.46a3.59,3.59,0,0,1-5.08,5.08L12,16.9A6.93,6.93,0,0,0,21.81,7.1Z" style="" fill="#E8710A"></path></g>
+</svg> <i class="bi bi-box-arrow-up-right"></i>`,
+            desc: "<strong>The link will open in new tab inside Google Colaboratory.</strong><br>Elevate your programming expertise in the Python Lab with code CSU1162P. This hands-on lab course is designed to immerse you in the practical aspects of Python programming. From basic syntax to complex problem-solving, you'll learn to write efficient and effective Python code. Click to dive into interactive sessions that will bolster your coding proficiency, enhance your algorithm design capabilities, and prepare you for real-world programming challenges.",
+            codetype: "CSU1162P | Lab with Google Colab"
             },
 
         ];
@@ -41,9 +44,9 @@ function get_menu_list(datetogen) {
                 link: "why-python"
             },
             {
-                title: "Introduction to Python: Unlocking the World of Programming",
-                desc: "Dive into the realm of Python programming, where we address a common problem: the overwhelming nature of starting to code. This article is designed to transform confusion into clarity, guiding beginners through Python's syntax, variables, data types, and operators. By demystifying these foundational concepts, we aim to equip you with the tools needed to solve real-world problems through coding. From writing your first script to understanding complex data structures, you'll discover how Python's simplicity and versatility make it an ideal language for newcomers and seasoned developers alike. Prepare to explore how each concept plays a critical role in building robust applications, setting the stage for a journey into Python's expansive ecosystem.",
-                codetype: "Getting Started with Python",
+                title: "Python in a Nutshell: Unlocking the World of Programming",
+                desc: "<strong>One Shot Article</strong><br>Dive into the realm of Python programming, where we address a common problem: the overwhelming nature of starting to code. This article is designed to transform confusion into clarity, guiding you through Python's syntax, function and its advanced counterparts, variables, data types, and operators. By demystifying these foundational concepts, we aim to equip you with the tools needed to solve real-world problems through coding. From writing your first script to understanding complex data structures, you'll discover how Python's simplicity and versatility make it an ideal language for newcomers and seasoned developers alike. Prepare to explore how each concept plays a critical role in building robust applications, setting the stage for a journey into Python's expansive ecosystem.",
+                codetype: "Python in a Nutshell",
                 readtime: 15,
                 link: "getting-started-with-python"
             },
