9e0833e44a1cd0d3a88f127ef2e930f0d565bb57
Divya Mohan <divyamohan1993@users.noreply.github.com>
2024-12-15 12:09:41 +0530

    english

--- edu/su/course/demo - Copy/var.js
+++ edu/su/course/demo - Copy/var.js
@@ -3,25 +3,20 @@ function get_menu_list(datetogen) {
     function get_main(def_date, def_author) {
         const arr = [
             {
-                link: "theory/",
-                title: "Theory: Course Overview and Concepts",
-                desc: "Description of the topic. This section provides an overview of the key concepts and principles related to the topic. Click to explore the fundamentals and advanced aspects of the subject, including real-world applications and case studies.",
-                codetype: "CSUCODE | Course",
-                cardimage: "https://cdn.dmj.one/edu/su/course/csuCODE/img/logo.webp",                
+                link: "#",
+                title: "Concepts and Principles of English Communication Seminar",
+                desc: "Explore the fundamental concepts and principles of English Communication in this section. Learn about the importance of effective communication, the elements of language, and the role of communication in various contexts. Click to delve into the basics of English Communication and enhance your language skills.",
+                codetype: "LSE005 | English Communication",
+                cardimage: "https://cdn.dmj.one/edu/su/course/cs/img/logo.webp",                
                 // cardimage_dark: "//cdn.dmj.one/img/needhelp.webp",               
                 // continuereading: " Click here to reach out to Faculty directly through email"
             },
-            {
-                link: "lab/",
-                title: "Lab: Hands-On Practice and Exercises",
-                desc: "Description of the topic. This section offers practical exercises and interactive tools to help you apply the concepts learned in the theory section. Click to access hands-on labs, coding exercises, and simulations that will enhance your understanding of the topic.",
-                codetype: "CSUCODE | Course",
-            },
+            
             {
                 link: "practice/",
-                title: "Practice Operating Systems Concepts: Strengthen Your Knowledge",
-                desc: "Reinforce your understanding of Operating Systems with practice exercises and interactive tools. This section offers a range of resources to help you master operating system concepts, including sorting algorithms, virtual machine installation, and more. Click to access hands-on exercises that will enhance your comprehension of operating system principles and prepare you for advanced topics in computer science.",
-                codetype: "Practice | Operating Systems",                
+                title: "Practice English Communication Skills: Strengthen Your Knowledge",
+                desc: "Reinforce your understanding of English Communication with practice exercises and interactive tools. This section offers a range of resources to help you master language skills, including grammar exercises, vocabulary building, and more. Click to access hands-on exercises that will enhance your comprehension of English Communication principles and prepare you for advanced topics in language studies.",
+                codetype: "Practice | English Communication",
             }
         ];
 
@@ -39,27 +34,7 @@ function get_menu_list(datetogen) {
         ];
         body_genmenu.processData(arr, def_date, def_author);
     }
-    function get_lab(def_date, def_author) {
-        const arr = [
-            // {
-            //     title: "Interactive Sorting Algorithms Visualizer",
-            //     desc: "Explore and visualize the workings of various sorting algorithms including Bubble Sort, Merge Sort, Quick Sort, and more. This interactive tool offers a step-by-step breakdown, allowing you to understand the mechanics and efficiency of each algorithm.",
-            //     author: "Divya Mohan",
-            //     link: "https://colab.research.google.com/drive/1goYusw-6vB2d2qZtxo4V1kt4mmLBrN11?usp=sharing",
-            //     // cardimage: "_url_"
-            // },
-            {
-                title: "Practical 1: Procedure for VirtualBox Installation and Configuration",
-                desc: "Learn to set up your operating system lab environment with VirtualBox, a powerful virtualization tool for creating and managing virtual machines. This practical exercise will guide you through the installation and configuration of VirtualBox, enabling you to create a virtualized environment for hands-on learning and experimentation.",
-                codetype: "Installation",
-                readtime: 2,
-                date: "Wed February 07, 2024",
-                link: "installing-virtualbox"              
-            },            
-        ];
-        body_genmenu.processData(arr, def_date, def_author);
-    }
-
+   
 
     /**********  AUTOMATION CONTROL **********/
     let title, link, date, desc, codetype, readtime, author = 1, semester, cardimage, continuereading;
