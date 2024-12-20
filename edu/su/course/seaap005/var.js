7e7ebed94be6c19a8df1e2a7c6b842554050dfd7
Divya Mohan <divyamohan1993@users.noreply.github.com>
2024-12-20 18:07:26 +0530

    aptitude - publish

--- edu/su/course/seaap005/var.js
+++ edu/su/course/seaap005/var.js
@@ -3,19 +3,13 @@ function get_menu_list(datetogen) {
     function get_main(def_date, def_author) {
         const arr = [
             {
-                link: "theory/",
-                title: "Theory: Course Overview and Concepts",
-                desc: "Description of the topic. This section provides an overview of the key concepts and principles related to the topic. Click to explore the fundamentals and advanced aspects of the subject, including real-world applications and case studies.",
-                codetype: "CSUCODE | Course",
-                cardimage: "https://cdn.dmj.one/edu/su/course/csuCODE/img/logo.webp",                
+                link: "theory/all-in-one",
+                title: "All in One: Concepts, and Questions",
+                desc: "This section provides a comprehensive overview of the topic, including key concepts, definitions, and examples. Click to access detailed explanations, practice questions, and interactive tools that will help you master the fundamentals of Operating Systems.",
+                codetype: "SEAAP005 | Aptitude Development",
+                cardimage: "https://cdn.dmj.one/edu/su/course/ap/img/logo.webp",                
                 // cardimage_dark: "//cdn.dmj.one/img/needhelp.webp",               
                 // continuereading: " Click here to reach out to Faculty directly through email"
-            },
-            {
-                link: "lab/",
-                title: "Lab: Hands-On Practice and Exercises",
-                desc: "Description of the topic. This section offers practical exercises and interactive tools to help you apply the concepts learned in the theory section. Click to access hands-on labs, coding exercises, and simulations that will enhance your understanding of the topic.",
-                codetype: "CSUCODE | Course",
             },
             {
                 link: "practice/",
@@ -38,34 +32,13 @@ function get_menu_list(datetogen) {
             },            
         ];
         body_genmenu.processData(arr, def_date, def_author);
-    }
-    function get_lab(def_date, def_author) {
-        const arr = [
-            // {
-            //     title: "Interactive Sorting Algorithms Visualizer",
-            //     desc: "Explore and visualize the workings of various sorting algorithms including Bubble Sort, Merge Sort, Quick Sort, and more. This interactive tool offers a step-by-step breakdown, allowing you to understand the mechanics and efficiency of each algorithm.",
-            //     author: "Divya Mohan",
-            //     link: "https://colab.research.google.com/drive/1goYusw-6vB2d2qZtxo4V1kt4mmLBrN11?usp=sharing",
-            //     // cardimage: "_url_"
-            // },
-            {
-                title: "Practical 1: Procedure for VirtualBox Installation and Configuration",
-                desc: "Learn to set up your operating system lab environment with VirtualBox, a powerful virtualization tool for creating and managing virtual machines. This practical exercise will guide you through the installation and configuration of VirtualBox, enabling you to create a virtualized environment for hands-on learning and experimentation.",
-                codetype: "Installation",
-                readtime: 2,
-                date: "Wed February 07, 2024",
-                link: "installing-virtualbox"              
-            },            
-        ];
-        body_genmenu.processData(arr, def_date, def_author);
-    }
+    }    
 
 
     /**********  AUTOMATION CONTROL **********/
     let title, link, date, desc, codetype, readtime, author = 1, semester, cardimage, continuereading;
     const functions = {
-        theory: get_class,
-        lab: get_lab,
+        theory: get_class,        
         default: get_main
     };
     (functions[window.urlpart5] || functions.default)(datetogen, author);
