4475b34da5a39e20c968fc9e0266fcd2b4c122f5
Divya Mohan <divyamohan1993@users.noreply.github.com>
2025-02-07 21:08:37 +0530

    Add Solution A implementation and update course details for Semester 6

--- js/comvar.js
+++ js/comvar.js
@@ -639,6 +639,8 @@ const courseDetailsMap = {
         'lab': ["Dr. Piyush Sewal", "CSU1899P", "Ethical Hacking Lab"]
     },
     'dmjcclt': { 'default': ["", "DMJCCLT", "Cloud Computing Concepts"] },
+
+    // Semester 6    
     'lse006': { 'default': ["Akshita Thakur", "LSE006", "English Communication Seminar 2"] },
     
 };
