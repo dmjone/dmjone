d8f500452d19b26892dd55e7d044f815d5851355
Divya Mohan <divyamohan1993@users.noreply.github.com>
2025-04-09 00:27:45 +0530

    csu1442

--- edu/su/course/csu1442/var.js
+++ edu/su/course/csu1442/var.js
@@ -12,12 +12,12 @@ function get_menu_list(datetogen) {
                 // continuereading: " Click here to reach out to Faculty directly through email"
             },
             
-            {
-                link: "practice/",
-                title: "Practice Concepts",
-                desc: "Practice your understanding of forensic psychology concepts with interactive quizzes, case studies, and real-world scenarios. These practice exercises will test your knowledge and help you apply key concepts to practical situations. Click to explore forensic psychology practice exercises and enhance your learning experience.",
-                codetype: "Practice | Forensic Psychology",                
-            }
+            // {
+            //     link: "practice/",
+            //     title: "Practice Concepts",
+            //     desc: "Practice your understanding of forensic psychology concepts with interactive quizzes, case studies, and real-world scenarios. These practice exercises will test your knowledge and help you apply key concepts to practical situations. Click to explore forensic psychology practice exercises and enhance your learning experience.",
+            //     codetype: "Practice | Forensic Psychology",                
+            // }
         ];
 
         body_genmenu.processData(arr, def_date, def_author, { isMainCategory: true });
@@ -25,42 +25,22 @@ function get_menu_list(datetogen) {
     function get_class(def_date, def_author) {
         const arr = [
             {
-                title: "Topic Name",
-                desc: "Description of the topic.",
+                title: "Unit 2: Criminal Behavior and Psychological Profiling",
+                desc: "Delve into the psychological aspects of criminal behavior and the techniques used in psychological profiling. This unit covers the theories and methods used to understand criminal minds, including the role of personality traits, mental disorders, and environmental factors in shaping criminal behavior.",
                 codetype: "Concepts",
                 readtime: 5,
-                link: "link-to-the-topic"
+                link: "criminal-behavior-and-psychological-profiling",
             },            
         ];
         body_genmenu.processData(arr, def_date, def_author);
     }
-    function get_lab(def_date, def_author) {
-        const arr = [
-            // {
-            //     title: "Interactive Sorting Algorithms Visualizer",
-            //     desc: "Explore and visualize the workings of various sorting algorithms including Bubble Sort, Merge Sort, Quick Sort, and more. This interactive tool offers a step-by-step breakdown, allowing you to understand the mechanics and efficiency of each algorithm.",
-            //     author: "Divya Mohan",
-            //     link: "https://colab.research.google.com/drive/1goYusw-6vB2d2qZtxo4V1kt4mmLBrN11?usp=sharing",
-            //     // cardimage: "_url_"
-            // },
-            {
-                title: "Practical 1: Procedure for VirtualBox Installation and Configuration",
-                desc: "Learn to set up your operating system lab environment with VirtualBox, a powerful virtualization tool for creating and managing virtual machines. This practical exercise will guide you through the installation and configuration of VirtualBox, enabling you to create a virtualized environment for hands-on learning and experimentation.",
-                codetype: "Installation",
-                readtime: 2,
-                date: "Wed February 07, 2024",
-                link: "installing-virtualbox"              
-            },            
-        ];
-        body_genmenu.processData(arr, def_date, def_author);
-    }
+    
 
 
     /**********  AUTOMATION CONTROL **********/
     let title, link, date, desc, codetype, readtime, author = 1, semester, cardimage, continuereading;
     const functions = {
-        theory: get_class,
-        lab: get_lab,
+        theory: get_class,    
         default: get_main
     };
     (functions[window.urlpart5] || functions.default)(datetogen, author);
