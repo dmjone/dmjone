28be16563f0112db6ef2ae8db4312bcdcce7238f
Divya Mohan <divyamohan1993@users.noreply.github.com>
2025-09-08 10:28:15 +0530

    Add new HTML files for Operating System course content and practicals
    - Created review.html for Placement Training Quiz on Operating Systems.
    - Added index.html for sub-index with dynamic title generation.
    - Introduced practicalindex-cc-cs.html for CSU360P Operating System Lab Practicals.
    - Added practicalindex-ds.html for another set of Operating System Lab Practicals.
    - Created sindex.html for Operating System overview.
    - Added theory/index.html for course theory content.
    - Introduced theory/topic.html for detailed topic structure and content.
    - Created var.js to manage menu list generation for course navigation.

---
+++ edu/su/course/csu358/var.js
@@ -0,0 +1,72 @@
+
+function get_menu_list(datetogen) {
+    function get_main(def_date, def_author) {
+        const arr = [
+            {
+                link: "theory/",
+                title: "Theory: Computer Graphics",
+                desc: "",
+                codetype: "CSU358 | Course",
+                // cardimage: "https://cdn.dmj.one/edu/su/course/csuCODE/img/logo.webp",                
+                // cardimage_dark: "//cdn.dmj.one/img/needhelp.webp",               
+                // continuereading: " Click here to reach out to Faculty directly through email"
+            },
+            // {
+            //     link: "lab/",
+            //     title: "Lab: Hands-On Practice and Exercises",
+            //     desc: "Description of the topic. This section offers practical exercises and interactive tools to help you apply the concepts learned in the theory section. Click to access hands-on labs, coding exercises, and simulations that will enhance your understanding of the topic.",
+            //     codetype: "CSUCODE | Course",
+            // },
+            // {
+            //     link: "practice/",
+            //     title: "Practice Operating Systems Concepts: Strengthen Your Knowledge",
+            //     desc: "Reinforce your understanding of Operating Systems with practice exercises and interactive tools. This section offers a range of resources to help you master operating system concepts, including sorting algorithms, virtual machine installation, and more. Click to access hands-on exercises that will enhance your comprehension of operating system principles and prepare you for advanced topics in computer science.",
+            //     codetype: "Practice | Operating Systems",                
+            // }
+        ];
+
+        body_genmenu.processData(arr, def_date, def_author, { isMainCategory: true });
+    }
+    function get_class(def_date, def_author) {
+        const arr = [
+            {
+                title: "Architecture of One Identity Manager [WIP]",
+                desc: "Dive into the architecture of One Identity Manager, a robust solution for identity and access management. This section covers the core components, data models, and workflows that make up the One Identity Manager framework. Understand how these elements work together to provide a comprehensive identity management solution in enterprise environments.",
+                codetype: "Concepts",
+                readtime: 5,
+                link: "architecture-of-one-identity-manager"
+            },            
+        ];
+        body_genmenu.processData(arr, def_date, def_author);
+    }
+    function get_lab(def_date, def_author) {
+        const arr = [
+            // {
+            //     title: "Interactive Sorting Algorithms Visualizer",
+            //     desc: "Explore and visualize the workings of various sorting algorithms including Bubble Sort, Merge Sort, Quick Sort, and more. This interactive tool offers a step-by-step breakdown, allowing you to understand the mechanics and efficiency of each algorithm.",
+            //     author: "Divya Mohan",
+            //     link: "https://colab.research.google.com/drive/1goYusw-6vB2d2qZtxo4V1kt4mmLBrN11?usp=sharing",
+            //     // cardimage: "_url_"
+            // },
+            {
+                title: "Practical 1: Procedure for VirtualBox Installation and Configuration",
+                desc: "Learn to set up your operating system lab environment with VirtualBox, a powerful virtualization tool for creating and managing virtual machines. This practical exercise will guide you through the installation and configuration of VirtualBox, enabling you to create a virtualized environment for hands-on learning and experimentation.",
+                codetype: "Installation",
+                readtime: 2,
+                date: "Wed February 07, 2024",
+                link: "installing-virtualbox"              
+            },            
+        ];
+        body_genmenu.processData(arr, def_date, def_author);
+    }
+
+
+    /**********  AUTOMATION CONTROL **********/
+    let title, link, date, desc, codetype, readtime, author = 1, semester, cardimage, continuereading;
+    const functions = {
+        theory: get_class,
+        lab: get_lab,
+        default: get_main
+    };
+    (functions[window.urlpart5] || functions.default)(datetogen, author);
+}
