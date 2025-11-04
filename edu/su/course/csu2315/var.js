6a1b0e31688b7218ea79b2d98671c3fa820ca724
Divya Mohan <divyamohan1993@users.noreply.github.com>
2025-11-04 22:31:00 +0530

    Add new course content for CSU2315: DevOps and Deployment
    - Created a new HTML file for the course topic, including structured data for SEO and a detailed table of contents.
    - Implemented a JavaScript file to generate menu lists for the course sections, including theory and practical exercises.
    - Updated the course list JSON to include the new course with relevant details such as competencies, semester, and difficulty level.

---
+++ edu/su/course/csu2315/var.js
@@ -0,0 +1,72 @@
+
+function get_menu_list(datetogen) {
+    function get_main(def_date, def_author) {
+        const arr = [
+            {
+                link: "theory/",
+                title: "Theory: DevOps and Deployment",
+                desc: "Explore the fundamental concepts of DevOps and Deployment in this section as you will gain insights into CI/CD, containerization, IaC, VC with Git and GitHub, configuration management with Ansible, monitoring and logging, and cloud deployment and automation.",
+                codetype: "CSU2315 | DevOps",
+                cardimage: "https://cdn.dmj.one/edu/su/course/csu2315/img/logo.webp",                
+                // cardimage_dark: "//cdn.dmj.one/img/needhelp.webp",               
+                // continuereading: " Click here to reach out to Faculty directly through email"
+            },
+            // {
+            //     link: "lab/",
+            //     title: "Lab: Hands-On Practice and Exercises",
+            //     desc: "Description of the topic. This section offers practical exercises and interactive tools to help you apply the concepts learned in the theory section. Click to access hands-on labs, coding exercises, and simulations that will enhance your understanding of the topic.",
+            //     codetype: "CSU2315 | DevOps",
+            // },
+            // {
+            //     link: "practice/",
+            //     title: "Practice Operating Systems Concepts: Strengthen Your Knowledge",
+            //     desc: "Reinforce your understanding of Operating Systems with practice exercises and interactive tools. This section offers a range of resources to help you master operating system concepts, including sorting algorithms, virtual machine installation, and more. Click to access hands-on exercises that will enhance your comprehension of operating system principles and prepare you for advanced topics in computer science.",
+            //     codetype: "Practice | Operating Systems",                
+            // }
+        ];
+
+        body_genmenu.processData(arr, def_date, def_author, { isMainCategory: true });
+    }
+    function get_class(def_date, def_author) {
+        const arr = [
+            {
+                title: "Intro to DevOps, Git, GitHub, Linux, EC2 and Docker",
+                desc: "An introduction to DevOps practices and tools including Git, GitHub, Linux, EC2, and Docker.",
+                codetype: "Concepts",
+                readtime: 5,
+                link: "intro-to-devops-git-github-linux-ec2-and-docker"
+            },            
+        ];
+        body_genmenu.processData(arr, def_date, def_author);
+    }
+    function get_lab(def_date, def_author) {
+        const arr = [
+            // {
+            //     title: "Interactive Sorting Algorithms Visualizer",
+            //     desc: "Explore and visualize the workings of various sorting algorithms including Bubble Sort, Merge Sort, Quick Sort, and more. This interactive tool offers a step-by-step breakdown, allowing you to understand the mechanics and efficiency of each algorithm.",
+            //     author: "Divya Mohan",
+            //     link: "https://colab.research.google.com/drive/1goYusw-6vB2d2qZtxo4V1kt4mmLBrN11?usp=sharing",
+            //     // cardimage: "_url_"
+            // },
+            {
+                title: "Practical 1: Procedure for VirtualBox Installation and Configuration",
+                desc: "Learn to set up your operating system lab environment with VirtualBox, a powerful virtualization tool for creating and managing virtual machines. This practical exercise will guide you through the installation and configuration of VirtualBox, enabling you to create a virtualized environment for hands-on learning and experimentation.",
+                codetype: "Installation",
+                readtime: 2,
+                date: "Wed February 07, 2024",
+                link: "installing-virtualbox"              
+            },            
+        ];
+        body_genmenu.processData(arr, def_date, def_author);
+    }
+
+
+    /**********  AUTOMATION CONTROL **********/
+    let title, link, date, desc, codetype, readtime, author = 1, semester, cardimage, continuereading;
+    const functions = {
+        theory: get_class,
+        lab: get_lab,
+        default: get_main
+    };
+    (functions[window.urlpart5] || functions.default)(datetogen, author);
+}
